0572d24cb2f090d82432b9c5ba8c4bf8b6b48173
kushii30 <khushihonakuppi@gmail.com>
2025-03-28 16:18:41 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+let a=100;
+ b=20;
+ a+=b;
+ console.log(a);
+ a-=b;
+ console.log(a);
+
+ let day=1;
+ switch (day){
+  case 1:
+   console.log("monday");
+   break;
+   case 2:
+    console.log("tuesday");
+    break;
+    default:
+ }
+
+
+
+
+  
